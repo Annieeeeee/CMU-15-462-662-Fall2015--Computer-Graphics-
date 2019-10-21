0e6398e25dca9dd1f392ae01c3e195b5cffa07da
Annieeeeee <34532259+Annieeeeee@users.noreply.github.com>
2019-10-21 16:41:26 +0800

    environment

---
+++ Assignment_1_DrawSVG/build/CMakeFiles/drawsvg.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/anthony/Documents/CMU-15-462-662-Fall2015--Computer-Graphics-/Assignment_1_DrawSVG/src/drawsvg.cpp" "/Users/anthony/Documents/CMU-15-462-662-Fall2015--Computer-Graphics-/Assignment_1_DrawSVG/build/CMakeFiles/drawsvg.dir/drawsvg.cpp.o"
+  "/Users/anthony/Documents/CMU-15-462-662-Fall2015--Computer-Graphics-/Assignment_1_DrawSVG/src/main.cpp" "/Users/anthony/Documents/CMU-15-462-662-Fall2015--Computer-Graphics-/Assignment_1_DrawSVG/build/CMakeFiles/drawsvg.dir/main.cpp.o"
+  "/Users/anthony/Documents/CMU-15-462-662-Fall2015--Computer-Graphics-/Assignment_1_DrawSVG/src/png.cpp" "/Users/anthony/Documents/CMU-15-462-662-Fall2015--Computer-Graphics-/Assignment_1_DrawSVG/build/CMakeFiles/drawsvg.dir/png.cpp.o"
+  "/Users/anthony/Documents/CMU-15-462-662-Fall2015--Computer-Graphics-/Assignment_1_DrawSVG/src/software_renderer.cpp" "/Users/anthony/Documents/CMU-15-462-662-Fall2015--Computer-Graphics-/Assignment_1_DrawSVG/build/CMakeFiles/drawsvg.dir/software_renderer.cpp.o"
+  "/Users/anthony/Documents/CMU-15-462-662-Fall2015--Computer-Graphics-/Assignment_1_DrawSVG/src/svg.cpp" "/Users/anthony/Documents/CMU-15-462-662-Fall2015--Computer-Graphics-/Assignment_1_DrawSVG/build/CMakeFiles/drawsvg.dir/svg.cpp.o"
+  "/Users/anthony/Documents/CMU-15-462-662-Fall2015--Computer-Graphics-/Assignment_1_DrawSVG/src/texture.cpp" "/Users/anthony/Documents/CMU-15-462-662-Fall2015--Computer-Graphics-/Assignment_1_DrawSVG/build/CMakeFiles/drawsvg.dir/texture.cpp.o"
+  "/Users/anthony/Documents/CMU-15-462-662-Fall2015--Computer-Graphics-/Assignment_1_DrawSVG/src/triangulation.cpp" "/Users/anthony/Documents/CMU-15-462-662-Fall2015--Computer-Graphics-/Assignment_1_DrawSVG/build/CMakeFiles/drawsvg.dir/triangulation.cpp.o"
+  "/Users/anthony/Documents/CMU-15-462-662-Fall2015--Computer-Graphics-/Assignment_1_DrawSVG/src/viewport.cpp" "/Users/anthony/Documents/CMU-15-462-662-Fall2015--Computer-Graphics-/Assignment_1_DrawSVG/build/CMakeFiles/drawsvg.dir/viewport.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/System/Library/Frameworks"
+  "/usr/local/include/freetype2"
+  "/Users/anthony/Documents/CMU-15-462-662-Fall2015--Computer-Graphics-/Assignment_1_DrawSVG/include"
+  "/Users/anthony/Documents/CMU-15-462-662-Fall2015--Computer-Graphics-/Assignment_1_DrawSVG/include/GLEW"
+  "/Users/anthony/Documents/CMU-15-462-662-Fall2015--Computer-Graphics-/Assignment_1_DrawSVG/include/GLFW"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
